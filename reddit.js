d6d49f31c834e00a8f99d3e3d11686f63d96be49
Ziad Saab <ziad.saab@gmail.com>
2016-07-19 13:16:32 -0400

    Update reddit.js

--- reddit.js
+++ reddit.js
@@ -12,7 +12,7 @@ module.exports = function RedditAPI(conn) {
         }
         else {
           conn.query(
-            'INSERT INTO `users` (`username`,`password`, `createdAt`) VALUES (?, ?, ?)', [user.username, hashedPassword, null],
+            'INSERT INTO users (username,password, createdAt) VALUES (?, ?, ?)', [user.username, hashedPassword, new Date()],
             function(err, result) {
               if (err) {
                 /*
@@ -34,7 +34,7 @@ module.exports = function RedditAPI(conn) {
                 and return it
                 */
                 conn.query(
-                  'SELECT `id`, `username`, `createdAt`, `updatedAt` FROM `users` WHERE `id` = ?', [result.insertId],
+                  'SELECT id, username, createdAt, updatedAt FROM users WHERE id = ?', [result.insertId],
                   function(err, result) {
                     if (err) {
                       callback(err);
@@ -60,7 +60,7 @@ module.exports = function RedditAPI(conn) {
     },
     createPost: function(post, callback) {
       conn.query(
-        'INSERT INTO `posts` (`userId`, `title`, `url`, `createdAt`) VALUES (?, ?, ?, ?)', [post.userId, post.title, post.url, null],
+        'INSERT INTO posts (userId, title, url, createdAt) VALUES (?, ?, ?, ?)', [post.userId, post.title, post.url, new Date()],
         function(err, result) {
           if (err) {
             callback(err);
@@ -71,7 +71,7 @@ module.exports = function RedditAPI(conn) {
             the post and send it to the caller!
             */
             conn.query(
-              'SELECT `id`,`title`,`url`,`userId`, `createdAt`, `updatedAt` FROM `posts` WHERE `id` = ?', [result.insertId],
+              'SELECT id,title,url,userId, createdAt, updatedAt FROM posts WHERE id = ?', [result.insertId],
               function(err, result) {
                 if (err) {
                   callback(err);
@@ -95,11 +95,11 @@ module.exports = function RedditAPI(conn) {
       var offset = (options.page || 0) * limit;
       
       conn.query(`
-        SELECT \`id\`,\`title\`,\`url\`,\`userId\`, \`createdAt\`, \`updatedAt\`
-        FROM \`posts\`
-        ORDER BY \`createdAt\` DESC
-        LIMIT ? OFFSET ?
-        `, [limit, offset],
+        SELECT id, title, url, userId, createdAt, updatedAt
+        FROM posts
+        ORDER BY createdAt DESC
+        LIMIT ? OFFSET ?`
+        , [limit, offset],
         function(err, results) {
           if (err) {
             callback(err);
