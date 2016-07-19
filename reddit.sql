672d1b8e8ca9f88d08fb09c667ff352a10532027
Ziad Saab <ziad.saab@gmail.com>
2016-07-19 13:06:52 -0400

    Update reddit.sql

--- reddit.sql
+++ reddit.sql
@@ -4,8 +4,8 @@ CREATE TABLE `users` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(50) NOT NULL,
   `password` VARCHAR(60) NOT NULL, -- why 60??? ask me :)
-  `createdAt` TIMESTAMP NOT NULL DEFAULT 0,
-  `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -17,8 +17,8 @@ CREATE TABLE `posts` (
   `title` varchar(300) DEFAULT NULL,
   `url` varchar(2000) DEFAULT NULL,
   `userId` int(11) DEFAULT NULL,
-  `createdAt` TIMESTAMP NOT NULL DEFAULT 0,
-  `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL
