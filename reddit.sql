63e04948850bede3baf7c3b1193dcc36b7ccfb41
Ziad Saab <ziad.saab@gmail.com>
2017-04-09 18:38:44 -0400

    simplified sql

--- reddit.sql
+++ reddit.sql
@@ -1,25 +1,23 @@
 -- This creates the users table. The username field is constrained to unique
 -- values only, by using a UNIQUE KEY on that column
-CREATE TABLE `users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(60) NOT NULL, -- why 60??? ask me :)
-  `createdAt` DATETIME NOT NULL,
-  `updatedAt` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(60) NOT NULL, -- why 60??? ask me :)
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NOT NULL,
+  UNIQUE KEY username (username)
 );
 
 -- This creates the posts table. The userId column references the id column of
 -- users. If a user is deleted, the corresponding posts' userIds will be set NULL.
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(300) DEFAULT NULL,
-  `url` varchar(2000) DEFAULT NULL,
-  `userId` int(11) DEFAULT NULL,
-  `createdAt` DATETIME NOT NULL,
-  `updatedAt` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `userId` (`userId`), -- why did we add this here? ask me :)
-  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL
+CREATE TABLE posts (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(300) DEFAULT NULL,
+  url VARCHAR(2000) DEFAULT NULL,
+  userId INT DEFAULT NULL,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NOT NULL,
+  KEY userId (userId), -- why did we add this here? ask me :)
+  CONSTRAINT validUser FOREIGN KEY (userId) REFERENCES users (id) ON DELETE SET NULL
 );
